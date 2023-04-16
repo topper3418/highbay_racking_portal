555c0bcb76139e09eacc7332900763aa4f571864
topper3418 <67675579+topper3418@users.noreply.github.com>
2023-04-16 14:19:54 -0700

    going back to desktop

--- testing/db_testing.py
+++ testing/db_testing.py
@@ -1,3 +1,9 @@
 # ok we are going to create a brand new database  file
+from data.db import database
+db = database('testing.db')
 # then we add data to the lookup tables
+def zip_org_dict(org, description):
+    org_headers = ['org', 'description']
+    return dict(zip(org_headers, [org, description]))
+org_lines = [('Production Control')]
 # then we access data in the lookup tables
