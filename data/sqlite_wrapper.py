27e65804932654d4fba862057feae17b744ea67e
topper3418 <67675579+topper3418@users.noreply.github.com>
2023-04-16 08:01:20 -0700

    moving downstairs now

--- data/sqlite_wrapper.py
+++
@@ -1,9 +0,0 @@
-#NULL: Represents a null or missing value.
-
-#INTEGER: Represents a signed integer value, which can be up to 8 bytes in size.
-
-#REAL: Represents a floating-point value, which can be up to 8 bytes in size.
-
-#TEXT: Represents a string of text, which can be up to 2^31-1 bytes in size.
-
-#BLOB: Represents a binary large object, which can be up to 2^31-1 bytes in size.
--- read_database.py
+++
@@ -1,8 +0,0 @@
-from main import app_data_path
-from data.sqlite_tools import Tickets
-
-tickets = Tickets(app_data_path)
-
-print(tickets.fetch_100())
-
-print(tickets.get_data('select * from tickets'))
